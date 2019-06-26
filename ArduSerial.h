6dfa308cf17b75192d0a7e4742562c9587a24b89
PB2 <ltbergeron@gmail.com>
2019-06-25 20:54:45 -0400

    Same as Cpp

--- ArduSerial.h
+++ ArduSerial.h
@@ -7,7 +7,7 @@
 
 
 
-const int ARDUINO_WAIT_TIME = 100;
+const int ARDUINO_WAIT_TIME = 2000;
 
 
 
@@ -15,12 +15,24 @@ const int ARDUINO_WAIT_TIME = 100;
 class WindowsSerial
 {
 public:
-	void begin(unsigned int baud, unsigned int comPortNum);
+	////////////////////////////////////////////////////////
+	// Default Constructor
+	WindowsSerial(unsigned int _comPortNum);
+
+	////////////////////////////////////////////////////////
+	// Initialization/Admin Stuff
+	void setPort(unsigned int _comPortNum);
+	void begin(unsigned int baud);
+	void begin(unsigned int baud, unsigned int _comPortNum);
 	bool connected();
 	void end();
 
+	////////////////////////////////////////////////////////
+	// Test For Available Data
 	unsigned int available();
 
+	////////////////////////////////////////////////////////
+	// I/O Functions
 	int read();
 	int read(char buffer[], unsigned int bufSize);
 
@@ -40,6 +52,10 @@ private:
 	bool isConnected;
 	COMSTAT status;
 	DWORD errors;
+
+	unsigned int comPortNum = NULL;
+
+	void initializePort(char portName[], unsigned int baud);
 };
 
 
@@ -52,3 +68,27 @@ extern WindowsSerial Serial3;
 extern WindowsSerial Serial4;
 extern WindowsSerial Serial5;
 extern WindowsSerial Serial6;
+extern WindowsSerial Serial7;
+extern WindowsSerial Serial8;
+extern WindowsSerial Serial9;
+extern WindowsSerial Serial10;
+extern WindowsSerial Serial11;
+extern WindowsSerial Serial12;
+extern WindowsSerial Serial13;
+extern WindowsSerial Serial14;
+extern WindowsSerial Serial15;
+extern WindowsSerial Serial16;
+extern WindowsSerial Serial17;
+extern WindowsSerial Serial18;
+extern WindowsSerial Serial19;
+extern WindowsSerial Serial20;
+extern WindowsSerial Serial21;
+extern WindowsSerial Serial22;
+extern WindowsSerial Serial23;
+extern WindowsSerial Serial24;
+extern WindowsSerial Serial25;
+extern WindowsSerial Serial26;
+extern WindowsSerial Serial27;
+extern WindowsSerial Serial28;
+extern WindowsSerial Serial29;
+extern WindowsSerial Serial30;
