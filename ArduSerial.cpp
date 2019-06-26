ec2dfeeec8f88651b5b48ba4295eb8fc2db087a9
PB2 <ltbergeron@gmail.com>
2019-06-25 20:54:12 -0400

    Bug fixes/added constructor/etc

--- ArduSerial.cpp
+++ ArduSerial.cpp
@@ -1,14 +1,63 @@
 #include "pch.h"
 #include "ArduSerial.h"
+#pragma warning(disable: 4996)
 
 
 
 
-void WindowsSerial::begin(unsigned int baud, unsigned int comPortNum)
+WindowsSerial::WindowsSerial(unsigned int _comPortNum)
+{
+	this->setPort(_comPortNum);
+}
+
+
+
+
+void WindowsSerial::setPort(unsigned int _comPortNum)
+{
+	this->comPortNum = _comPortNum;
+}
+
+
+
+
+void WindowsSerial::begin(unsigned int baud)
 {
 	/*
-	char port_name[] = "\\\\.\\COM20";
+	#define CBR_110             110
+	#define CBR_300             300
+	#define CBR_600             600
+	#define CBR_1200            1200
+	#define CBR_2400            2400
+	#define CBR_4800            4800
+	#define CBR_9600            9600
+	#define CBR_14400           14400
+	#define CBR_19200           19200
+	#define CBR_38400           38400
+	#define CBR_56000           56000
+	#define CBR_57600           57600
+	#define CBR_115200          115200
+	#define CBR_128000          128000
+	#define CBR_256000          256000
+	*/
 
+	if (this->comPortNum != NULL)
+	{
+		char format[] = "\\\\.\\COM%i";
+		char portName[20];
+
+		snprintf(portName, 12, format, this->comPortNum);
+
+		initializePort(portName, baud);
+	}
+}
+
+
+
+
+void WindowsSerial::begin(unsigned int baud, unsigned int _comPortNum)
+{
+	/*
 	#define CBR_110             110
 	#define CBR_300             300
 	#define CBR_600             600
@@ -29,16 +78,25 @@ void WindowsSerial::begin(unsigned int baud, unsigned int comPortNum)
 	char format[] = "\\\\.\\COM%i";
 	char portName[20];
 
-	snprintf(portName, 12, format, comPortNum);
+	setPort(_comPortNum);
+	snprintf(portName, 12, format, this->comPortNum);
 
+	initializePort(portName, baud);
+}
+
+
+
+
+void WindowsSerial::initializePort(char portName[], unsigned int baud)
+{
 	this->isConnected = false;
-	this->handler = CreateFileA(static_cast<LPCSTR>(portName),
-								GENERIC_READ | GENERIC_WRITE,
-								0,
-								NULL,
-								OPEN_EXISTING,
-								FILE_ATTRIBUTE_NORMAL,
-								NULL);
+	this->handler = CreateFileA(static_cast<LPCSTR>(portName), // lpFileName
+		GENERIC_READ | GENERIC_WRITE,  // dwDesiredAccess
+		0,                             // dwShareMode
+		NULL,                          // lpSecurityAttributes
+		OPEN_EXISTING,                 // dwCreationDisposition
+		FILE_ATTRIBUTE_NORMAL,         // dwFlagsAndAttributes
+		NULL);                         // hTemplateFile
 
 	if (this->handler == INVALID_HANDLE_VALUE)
 	{
@@ -66,7 +124,14 @@ void WindowsSerial::begin(unsigned int baud, unsigned int comPortNum)
 			else
 			{
 				this->isConnected = true;
-				PurgeComm(this->handler, PURGE_RXCLEAR | PURGE_TXCLEAR);
+
+				// flush any remaining characters in the buffers 
+				PurgeComm(this->handler, PURGE_RXABORT |
+					PURGE_RXCLEAR |
+					PURGE_TXABORT |
+					PURGE_TXCLEAR);
+
+				// wait as the arduino board will be reseting
 				Sleep(ARDUINO_WAIT_TIME);
 			}
 		}
@@ -137,9 +202,11 @@ int WindowsSerial::read(char buffer[], unsigned int bufSize)
 bool WindowsSerial::print(const std::string message)
 {
 	DWORD bytesSend;
-	char buffer[100];
+	size_t bufSize = message.size();
 
-	unsigned int bufSize = sprintf(buffer, "%s", message);
+	char *buffer = new char[message.size() + 1];
+	message.copy(buffer, message.size() + 1);
+	buffer[message.size()] = '\0';
 
 	if (!WriteFile(this->handler, buffer, bufSize, &bytesSend, 0))
 	{
@@ -262,3 +329,38 @@ bool WindowsSerial::connected()
 {
 	return this->isConnected;
 }
+
+
+
+
+WindowsSerial Serial(0);
+WindowsSerial Serial1(1);
+WindowsSerial Serial2(2);
+WindowsSerial Serial3(3);
+WindowsSerial Serial4(4);
+WindowsSerial Serial5(5);
+WindowsSerial Serial6(6);
+WindowsSerial Serial7(7);
+WindowsSerial Serial8(8);
+WindowsSerial Serial9(9);
+WindowsSerial Serial10(10);
+WindowsSerial Serial11(11);
+WindowsSerial Serial12(12);
+WindowsSerial Serial13(13);
+WindowsSerial Serial14(14);
+WindowsSerial Serial15(15);
+WindowsSerial Serial16(16);
+WindowsSerial Serial17(17);
+WindowsSerial Serial18(18);
+WindowsSerial Serial19(19);
+WindowsSerial Serial20(20);
+WindowsSerial Serial21(21);
+WindowsSerial Serial22(22);
+WindowsSerial Serial23(23);
+WindowsSerial Serial24(24);
+WindowsSerial Serial25(25);
+WindowsSerial Serial26(26);
+WindowsSerial Serial27(27);
+WindowsSerial Serial28(28);
+WindowsSerial Serial29(29);
+WindowsSerial Serial30(30);
