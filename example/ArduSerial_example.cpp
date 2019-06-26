2a3c114abd7a967b142fc9e5473c778484c592ab
PB2 <ltbergeron@gmail.com>
2019-06-25 20:56:03 -0400

    Update ArduSerial_example.cpp

--- example/ArduSerial_example.cpp
+++ example/ArduSerial_example.cpp
@@ -1,27 +1,59 @@
 #include "pch.h"
+#include <iostream>
 #include "ArduSerial.h"
+
+
+
+std::string data = "hi";
+
+
+
+
+void setup()
+{
+	// Serial27 means access Arduino on COM27 ***************
+	Serial27.begin(115200);
+
+	std::cout << "Starting..." << std::endl;
+
+	while (!Serial27.connected());
+
+	while (Serial27.available())
+		Serial27.read();
+
+	std::cout << "Connected" << std::endl;
+}
+
+
+
+
+void loop()
+{
+	std::cout << "Sending data: " << data << std::endl;
+	Serial27.println(data);
+
+	std::cout << "Looking for echo..." << std::endl;
+	while (Serial27.available() < data.length());
+
+	std::cout << "Found the following echo:" << std::endl;
+	while (Serial27.available())
+	{
+		printf("%c", Serial27.read());
+	}
+
+	std::cout << std::endl << "Waiting before transmitting again..." << std::endl;
+	Sleep(1000);
+}
 
 
 
 
 int main()
 {
-	Serial1.begin(115200, 25);
-	while (!Serial1.connected());
+	setup();
 
-	if (Serial1.connected())
-	{
-		Serial1.write('h');
-		Serial1.write('i');
-		Serial1.write('\n');
-	}
-
-	while (Serial1.available() < 3);
-
-	while (Serial1.available())
-	{
-		printf("%c", Serial1.read());
-	}
+	while (true)
+		loop();
 
 	return 0;
 }
